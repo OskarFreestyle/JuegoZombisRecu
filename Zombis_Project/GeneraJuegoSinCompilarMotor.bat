3b11e4ec749c2ecb6f03ad47451b486db63b2fd0
OskarFreestyle <oscafe01@ucm.es>
2022-06-13 18:50:49 +0200

    Cambios debidos al nuevo nombre del repositorio
    Co-Authored-By: oscarpb99 <36916863+oscarpb99@users.noreply.github.com>

--- Zombis_Project/GeneraJuegoSinCompilarMotor.bat
+++ Zombis_Project/GeneraJuegoSinCompilarMotor.bat
@@ -3,7 +3,7 @@
 
 :: Primero tiene que construir el Motor
 echo BUILDING MOTOR
-cd Motor-Ola/MotorOla_Solution
+cd Motor-Ola-Recu/MotorOla_Solution
 ::call MotorOla.bat
 mkdir GameToLoad
 cd ../..
@@ -11,18 +11,18 @@ cd ../..
 :: Despues tiene que generar Game.dll y Game.lib
 echo BUILDING GAME
 msbuild Zombis_Project.sln -p:Configuration=Debug
-msbuild Zombis_Project.sln -p:Configuration=Release
+::msbuild Zombis_Project.sln -p:Configuration=Release
 echo BUILDING DONE
 
 :: Ahora hay que mover Juego.dll a Motor-Ola/MotorOla_Solution/GameToLoad
 cd DLL
-copy /Y Juego.dll "../Motor-Ola/MotorOla_Solution/Exes/Juego.dll"
-copy /Y Juego_d.dll "../Motor-Ola/MotorOla_Solution/Exes/Juego_d.dll"
+copy /Y Juego.dll "../Motor-Ola-Recu/MotorOla_Solution/Exes/Juego.dll"
+copy /Y Juego_d.dll "../Motor-Ola-Recu/MotorOla_Solution/Exes/Juego_d.dll"
 echo JUEGO.DLL MOVIDO
 cd ..
 
 :: Y los Assets del Juego a Motor-Ola/MotorOla_Solution/Exes/Assets
-xcopy /Y /s Assets "./Motor-Ola/MotorOla_Solution/Exes/Assets"
+xcopy /Y /s Assets "./Motor-Ola-Recu/MotorOla_Solution/Exes/Assets/"
 echo ASSETS MOVIDOS
 
 :: Pausa para ver que ha ocurrido
